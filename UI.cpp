209d9a151ecd1de1b73bf9285baafa9065c49816
gy <151158496+gy2649263674@users.noreply.github.com>
2024-04-28 22:54:14 +0800

    Add files via upload

---
+++ UI.cpp
@@ -0,0 +1,10 @@
+﻿#include"all.h"
+
+Widget::Widget(int x, int y, int w, int h)
+{
+	initgraph(x, y,0 | EX_DBLCLKS);
+}
+Widget::Widget(int x, int y)
+{
+	initgraph(x, y, 0 | EX_DBLCLKS);
+}
---
+++ role.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include"graph.h"
+//class graph;
+enum State {
+	L = 0,
+	R
+};
+enum Direction
+{
+	Up,
+	Down,
+	Left,
+	Right
+};
+class Role
+{
+public:
+	Role(const char* character_name, int image_num);
+	Role(void);
+	const char* get_role_name() const;
+	int get_image_num() const;
+	int getx() const;
+	int gety() const;
+	const IMAGE* get_frame(int index) const;
+	void rotate();
+	int get_frame();
+	void add_atlas();
+private:
+	int pic_index = 0;
+	int x = 0, y = 0;
+	std::string role_name;
+	int image_num;
+	int speed = 0;
+	int HP = 100;
+	State state;
+	//Atlas* role_image;
+};
