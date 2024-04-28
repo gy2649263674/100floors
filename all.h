d308bd2b6e10f73b405add01c3392a5669a06cdf
gy <2649263674@qq.com>
2024-04-29 00:12:26 +0800

    clear

--- all.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include<iostream>
-#include<easyx.h>
-#include<graphics.h>
-#include"graph.h"
-#include"role.h"
-#include<string>
---
+++ code/head/all.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include<iostream>
+#include<easyx.h>
+#include<graphics.h>
+#include"graph.h"
+#include"role.h"
+#include<string>
